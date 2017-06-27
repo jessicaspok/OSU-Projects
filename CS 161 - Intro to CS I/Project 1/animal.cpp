68618512e050c6a7af774037c2b12b5c50df7b0c
spokoynj <spokoynj@oregonstate.edu>
2017-06-27 13:04:12 +0800

    cs 161 proj1

---
+++ CS 161 - Intro to CS I/Project 1/animal.cpp
@@ -0,0 +1,20 @@
+/********************************************************************************* Author: Jessica Spokoyny
+** Date: June 26, 2015
+** Description: Assignment 1 - Favorite Animal
+*******************************************************************************/
+
+#include <iostream>
+#include <string>
+
+// a simple example program
+int main()
+{
+   std::string faveAnimal;
+   std::cout << "Please enter your favorite animal." << std::endl;
+   std::cin >> faveAnimal;
+   std::cout << "Your favorite animal is the " << faveAnimal;
+   std::cout << "." << std::endl;
+
+   return 0;
+}
+
